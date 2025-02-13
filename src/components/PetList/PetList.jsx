04ed4a1077fea5212d71e76b98b5f48043c564ce
jimbojones1 <jimmyhaff3@gmail.com>
2025-02-13 15:46:25 -0500

    finished react-router

--- src/components/PetList/PetList.jsx
+++ src/components/PetList/PetList.jsx
@@ -3,17 +3,22 @@ import './Petlist.css'
 // import it into the file to be rendered (aka App.js)
 // confirm your props in the devtools before you use them!
 
+import { Link } from 'react-router'
+
 export default function PetList(props){
 
     const petLis = props.pets.map((pet) => {
-        return <li key={pet._id} onClick={() => props.setSelectedPet(pet)}>{pet.name}</li>
+        return (
+        <li key={pet._id} >
+            <Link to={`/pets/${pet._id}`}>{pet.name}</Link>
+        </li>
+        )
     })
 
     // check to see if we have pets
     return (
         <section className={'pet-list'}>
             <h1>Pet List</h1>
-            <button onClick={props.handleFormOpen}>{props.buttonTextForForm}</button>
             {petLis.length !== 0 ? (
                 <ul>
                     {petLis}
